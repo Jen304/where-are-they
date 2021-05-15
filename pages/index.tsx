e0c0097a05bfdd5bc206fbe03c42732a21dc562f
Jen <jenhoang304@gmail.com>
2021-05-15 15:53:23 -0700

    Adjust layout background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import Layout from "../components/layout";
 import DefaultHeader from "../components/default-header";
-import GameStarterCard from "../components/game-starter-card";
+import GameStarterCard from "../components/starter-card";
 import { ReactElement } from "react";
 
 const Home = (): ReactElement => {
   return (
     <Layout header={<DefaultHeader />}>
       <GameStarterCard />
-      <h1>hello</h1>
     </Layout>
   );
 };
