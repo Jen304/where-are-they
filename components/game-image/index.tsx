2023973716529349cec1a30ccdff8d9d0ffa4e07
Jen <jenhoang304@gmail.com>
2021-05-15 20:34:38 -0700

    Add custom-cursor to GameImage component

--- components/game-image/index.tsx
+++ components/game-image/index.tsx
@@ -1,8 +1,26 @@
 import { ReactElement } from "react";
 import styles from "./game-image.module.css";
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
 
 const GameImage = (): ReactElement => {
-  return <img className={styles.gameImage} src="game-picture.jpg"></img>;
+  return (
+    <div className={styles.gameImageContainer}>
+      <img className={styles.gameImage} src="game-picture.jpg"></img>
+      <CustomCursor
+        targets={[".link", ".your-css-selector"]}
+        customClass="custom-cursor"
+        dimensions={120}
+        fill="#FFF"
+        smoothness={{
+          movement: 0.2,
+          scale: 0.1,
+          opacity: 0.3,
+        }}
+        targetOpacity={0.5}
+      />
+    </div>
+  );
 };
 
 export default GameImage;
